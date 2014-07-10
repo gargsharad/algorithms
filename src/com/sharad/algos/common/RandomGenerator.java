cfbd222a42b2aa2b836f1e41e931e6439125c667
gargsharad <garg.sharadkumar@gmail.com>
2014-07-10 13:14:04 -0700

    adding common random number generator for input

---
+++ src/com/sharad/algos/common/RandomGenerator.java
@@ -0,0 +1,14 @@
+package com.sharad.algos.common;
+
+import java.util.Random;
+
+public class RandomGenerator {
+	public static Integer[] getRandomIntegers(Integer range) {
+		Integer[] randomInts = new Integer[range];
+		Random randomGenerator = new Random();
+		for (int idx = 0; idx < range; ++idx) {
+			randomInts[idx] = randomGenerator.nextInt(1000);
+		}
+		return randomInts;
+	}
+}
