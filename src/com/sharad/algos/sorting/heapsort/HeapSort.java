c5b79368eb2234cf7fbc4fb2b8a701b91fbe039c
gargsharad <garg.sharadkumar@gmail.com>
2014-10-29 16:52:06 -0700

    heap sort impl

--- src/com/sharad/algos/sorting/heapsort/HeapSort.java
+++ src/com/sharad/algos/sorting/heapsort/HeapSort.java
@@ -4,100 +4,93 @@ import com.sharad.algos.common.RandomGenerator;
 import com.sharad.algos.common.Utility;
 
 /*
-
  The binary heap data structures is an array that can be viewed as a complete binary tree. 
  Each node of the binary tree corresponds to an element of the array. The array is comple-
  -tely filled on all levels except possibly lowest.
- 
+
  The root of the tree A[0] and given index i of a node, the indices of its parent, left c-
  -hild and right child can be computed
-    PARENT 	of node at index i will be at index = (i-1)/2
-    LEFT  	child of the node will be at index = 2i+1
-    RIGHT 	child of the node will be at index = 2i+2
-	lets say the length of a given array is 10 and we have 5 elements in it
-	then by the definition of parent
-	
-	parent of element at index 7 will be 3rd leaf 
-	parent of element at index 6 will be 3rd leaf 
-	parent of element at index 5 will be 2nd leaf
-	parent of element at index 4 will be 2nd leaf
-	parent of element at index 3 rooted at index 1  and parent of 6 and 7
-	parent of element at index 2 rooted at index 0  and parent of 5 and 6
-	parent of element at index 1 rooted at index 0  and parent of 3 and 4
-	parent of element at index 0 will root node and parent of 1 and 2
-	
-	So all the nodes in the array with index (length/2)+1 are leaf nodes use this fact to 
-	create the procedure of heapify.
-	
-	start with the index of last non leaf node 
-		
-	start from the index of last non null element of array.
-	find its left child if exists 
-		compare its value with child value if parent less then child then swap
-		assign value of child index to root index and repeat
-	find its right child if exists
-		compare its value with child value if parent less then child then swap
-		assign value of child index to root index and repeat
-	repeat until we reach index 0 root of the tree  
-	 
- */
+	 PARENT 	of node at index i will be at index = (i)/2
+	 LEFT  	child of the node will be at index = 2i+1
+	 RIGHT 	child of the node will be at index = 2i+2
+
+ Heapify process:
+	 Heapify is a procedure for manipulating heap data structures. It is given an array 
+	 A and index i into the array. The subtree rooted at the children of A[i] are heap 
+	 but node A[i] itself may possibly violate the heap property i.e., A[i] < A[2i] or A[i] < A[2i +1].
+	 The procedure 'Heapify' manipulates the tree rooted at A[i] so it becomes a heap. 
+	 In other words, 'Heapify' is let the value at A[i] "float down" in a heap so that 
+	 subtree rooted at index i becomes a heap.
+
+ Heapsort: Create heap first
+	 while(n>=1){
+		 get the largest element [at index 0 ]from heap store swap it with last 
+		 element of array [at index n-1]
+		 n=n-1
+		 run heapify for the elements in the sub array array[0, n-1]
+ 	} 	 
+*/
 
 public class HeapSort {
-	// We can use the procedure 'Heapify' in a bottom-up fashion to convert an
-	// array A[1 . . n] into a heap. Since the elements in the subarray A[n/2 +1
-	// . . n] are all leaves, the procedure BUILD_HEAP goes through the
-	// remaining nodes of the tree and runs 'Heapify' on each one. The bottom-up
-	// order of processing node guarantees that the subtree rooted at children
-	// are heap before 'Heapify' is run at their parent.
 	public static void main(String[] args) {
 		HeapSort heapSort = new HeapSort();
 		System.out.println("Starting Heap sort ..........");
-		Integer[] input = RandomGenerator.getRandomIntegers(2000);
+		Integer[] input = RandomGenerator.getRandomIntegers(10);
+		int size = 0;
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] != null)
+				size += 1;
+		}
 		System.out.println("Unsorted  input array........");
 		Utility.printArray(input);
-		heapSort.sort(input);
+		heapSort.sort(input, size);
 		System.out.println("Sorted Output array .........");
 		Utility.printArray(input);
 		System.out.println("Exiting Heap sort ...........");
 	}
 
-	private void sort(Integer[] input) {
-		buildHeap(input);
-		/*int temp;
-		for (int i = input.length - 1; i >= 1; i--) {
+	private void sort(Integer[] input, int size) {
+		buildHeap(input, size);
+		System.out.println("Printing Heap ...........");
+		Utility.printArray(input);
+		int temp;
+		for (int i = size - 1; i >= 1; i--) {
 			temp = input[0];
 			input[0] = input[i];
 			input[i] = temp;
 			heapify(input, 0, i - 1);
-		}*/
-	}
-
-	private void buildHeap(Integer[] input) {
-		for (int i = (input.length/2) - 1; i >= 0; i--) {
-			heapify(input, i, input.length-1);
 		}
 	}
 
+	private void buildHeap(Integer[] input, int size) {
+		for (int i = (((size) - 1) / 2); i >= 0; i--) {
+			heapify(input, i, size - 1);
+		}
+	}
 	private void heapify(Integer[] input, int rootIndex, int bottomIndex) {
 		int maxChild, temp;
 		boolean done = false;
-		while ((rootIndex * 2 <= bottomIndex) && (!done)) {
-
-			if (rootIndex * 2 == bottomIndex) {
-				maxChild = rootIndex * 2;
+		System.out.println("?????");
+		while (((rootIndex * 2) <= bottomIndex) && (!done)) {
+			System.out.println("Node at root :: "+ rootIndex + "      "+ input[rootIndex]);
+			if ((rootIndex * 2) == bottomIndex) {
+				maxChild = (rootIndex * 2);
 			} else if (input[rootIndex * 2] > input[rootIndex * 2 + 1]) {
 				maxChild = rootIndex * 2;
 			} else {
 				maxChild = rootIndex * 2 + 1;
 			}
-
 			if (input[rootIndex] < input[maxChild]) {
+				System.out.println("Node are swapped with values :: root index :: "+rootIndex+" value ::: " + input[rootIndex]+ "   Max child index ::: "+ maxChild+" value ::: "+ input[maxChild]);
 				temp = input[rootIndex];
 				input[rootIndex] = input[maxChild];
 				input[maxChild] = temp;
-				rootIndex = maxChild; //--- this is the key
+				// --- this is the key; after swapping we need to check further whether 
+				// the heap property is satisfied further down the heap
+				rootIndex = maxChild;
 			} else {
 				done = true;
+				System.out.println("################");
 			}
 		}
 	}
